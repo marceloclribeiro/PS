d25e14d281a4e5148e186fee2f4506f4a030d558
edlcorrea <playered.lc@gmail.com>
2021-04-29 12:11:20 -0300

    add GUI Package and initial files.

---
+++ src/gui/App.java
@@ -0,0 +1,25 @@
+package gui;
+
+/**
+ *
+ * @author edlcorrea
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void launchGUI(String[] args) {
+        launch(args);
+    }    
+}
---
+++ src/gui/MainViewController.java
@@ -0,0 +1,10 @@
+package gui;
+
+/**
+ *
+ * @author edlcorrea
+ */
+
+public class MainViewController {
+    
+}
