21ac535eb107b5eaa7fc47ebc740e61c8c35de84
edlcorrea <playered.lc@gmail.com>
2021-05-01 11:37:09 -0300

    set the overall layout.

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,9 +1,5 @@
 package gui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-
 /**
  *
  * @author edlcorrea
@@ -11,23 +7,4 @@ import javafx.scene.control.Label;
 
 public class MainViewController {
     
-    @FXML
-    private Label testLabel;
-    @FXML
-    private Button testButton;
-    
-    public void checkSync() {
-        updateLabel("Sync.");
-        updateButton("Checked");
-        disableButton();
-    }
-    public void updateLabel(String labelText) {
-        testLabel.setText(labelText);
-    }
-    public void updateButton(String bttnText) {
-        testButton.setText(bttnText);
-    }
-    public void disableButton() {
-        testButton.setDisable(true);
-    }
 }
