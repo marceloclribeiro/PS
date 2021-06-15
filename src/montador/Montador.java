69de0859205a76500bf4a3c18cb7e805bf8a439c
thomaziogiacobbe <tgiacobbe@outlook.com>
2021-06-15 15:58:27 -0300

    ta quase pronto o assembler (aleluia)

--- src/montador/Montador.java
+++ src/montador/Montador.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.HashMap;
+import logic.String24;
 
 /**
  *
@@ -21,6 +22,8 @@ public class Montador {
     
     private static ArrayList<String> instructions = new ArrayList<>(Arrays.asList("ADDR","CLEAR","COMPR","DIVR","MULR","RMO","SHIFTL","SHIFTR","SUBR","TIXR","ADD","AND","COMP","DIV","J","JEQ","JGT","JLT","JSUB","LDA","LDB","LDCH","LDL","LDS","LDT","LDX","MUL","OR","RSUB","STA","STB","STCH","STL","STS","STT","STX","SUB","TIX","WORD","END"));
     // <= 9 -> 2 bytes
+    private static ArrayList<Integer> instructions_opcode = new ArrayList<>(Arrays.asList(144,4,160,156,152,172,164,168,148,184,6,16,10,9,15,12,13,14,18,0,26,20,2,27,29,1,8,17,19,3,30,21,5,31,33,4,7,11,18));
+    
     private static HashMap<String, Integer> tabelaDeLabels = new HashMap<>(); 
     
     public static void assembler(File sic){
@@ -40,7 +43,6 @@ public class Montador {
             }       
             
             int i = 0;
-            
             for (String c : conteudo){
                 boolean isLabel;
                 String[] word = c.split(" ");
@@ -77,6 +79,188 @@ public class Montador {
                 }                
             }
             
+            i = 0;
+            //segunda passada
+            for (String c : conteudo){
+                String[] word = c.split(" ");
+                String[] linha = null;
+                if (tabelaDeLabels.containsKey(word[0])){
+                    linha = Arrays.copyOfRange(word, 1, word.length);
+                } else {
+                    linha = word;
+                }
+                int instsize;
+                
+                boolean isExtended;
+                if (linha[0].charAt(0) == '+'){
+                    isExtended = true;
+                } else {
+                    isExtended = false;
+                }
+                int op;
+                String24 opBinary;
+                
+                op = instructions.indexOf(linha[0].replace("+", ""));
+                if (isExtended == true){
+                    instsize = 4;
+                } else if (op <= 9){
+                    instsize = 2;
+                } else if (op != instructions.size() - 1){
+                    instsize = 3;
+                } else {
+                    instsize = 1;
+                }
+                op = instructions_opcode.get(op);
+                
+                
+//                switch(linha[0].replace("+", "")){
+//                    //2 bytes
+//                    case "ADDR":
+//                        instsize = 2;
+//                        op = instructions_opcode.get(0);
+//                        break;
+//                        
+//                    case "CLEAR":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    case "COMPR":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    case "DIVR":
+//                        instsize = 2;
+//                        break;
+//                    
+//                    case "MULR":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    case "RMO":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    case "SHIFTL":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    case "SHIFTR":
+//                        instsize = 2;
+//                        break;
+//                    
+//                    case "SUBR":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    case "TIXR":
+//                        instsize = 2;
+//                        break;
+//                        
+//                    
+//                    //3/4 bytes
+//                    case "ADD":
+//                        break;
+//                        
+//                    case "AND":
+//                        break;
+//                        
+//                    case "COMP":
+//                        break;
+//                    
+//                    case "DIV":
+//                        break;
+//                        
+//                    case "J":
+//                        break;
+//                        
+//                    case "JEQ":
+//                        break;
+//                    
+//                    case "JGT":
+//                        break;
+//                    
+//                    case "JLT":
+//                        break;
+//                        
+//                    case "JSUB":
+//                        break;
+//                        
+//                    case "LDA":
+//                        break;
+//                        
+//                    case "LDB":
+//                        break;
+//                        
+//                    case "LDCH":
+//                        break;
+//                        
+//                    case "LDL":
+//                        break;
+//                        
+//                    case "LDS":
+//                        break;
+//                        
+//                    case "LDT":
+//                        break;
+//                        
+//                    case "LDX":
+//                        break;
+//                        
+//                    case "MUL":
+//                        break;
+//                        
+//                    case "OR":
+//                        break;
+//                        
+//                    case "RSUB":
+//                        break;
+//                        
+//                    case "STA":
+//                        break;
+//                        
+//                    case "STB":
+//                        break;
+//                        
+//                    case "STCH":
+//                        break;
+//                        
+//                    case "STL":
+//                        break;
+//                        
+//                    case "STS":
+//                        break;
+//                        
+//                    case "STT":
+//                        break;
+//                        
+//                    case "STX":
+//                        break;
+//                        
+//                    case "SUB":
+//                        break;
+//                        
+//                    case "TIX":
+//                        break;
+//                        
+//                        
+//                        
+//                    //outras
+//                    case "WORD":
+//                        break;
+//                        
+//                    case "END":
+//                        break;
+//                        
+//                        
+//                        
+//                    default:
+//                        System.out.println("ERRO! A instrucao " + linha[0] + " nao existe");
+//                        System.out.println("Linha: " + i);
+//                        System.exit(1);
+//                        break;
+//                }
+                i += instsize;
+            }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Montador.class.getName()).log(Level.SEVERE, null, ex);
         }
